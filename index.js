79931063a5466046fb8e67532f8d95bf13758f80
Anderson <falarcomanderson@yahoo.com.br>
2024-04-04 19:07:59 -0300

    Tentando

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
 const prompt = require ('prompt-sync') ( );
 
-let pessoaMesa = parseInt(prompt("Insira a quantidade de pessoas na mesa! :"));
+// let pessoaMesa = parseInt(prompt("Insira a quantidade de pessoas na mesa! - "));
+let pessoaMesa = "";
+while(!Number.isInteger(pessoaMesa) ) {
+    pessoaMesa = parseInt(prompt("Insira a quantidade de pessoas na mesa! - "));   
+}
 let totalConta = parseFloat(prompt("Insira o total da conta! :"));
 let metodoPagamento = parseInt(prompt("Insira o metodo de pagamento (1 - Pix / 2 - Dinheiro / 3 - Cartão) : "));
+let divideConta = parseInt(prompt("Deseja dividir o total da conta? 1 = Sim  /  2 = Não : "));
+let calculaGorjeta = parseInt(prompt("Deseja colaborar com uma gorjeta para o garcom? 1 - Sim / 2 - Não  :"));
+let valorGorjeta = 0;
+if (calculaGorjeta == 1){
+    valorGorjeta = parseFloat(prompt("Insira o valor da gorjeta: R$ "));
+    
+    }
 
-
-let totalComDesconto = (totalConta-(totalConta*10)/100);
-//console.log(totalComDesconto.toFixed(2));
-
-switch(metodoPagamento){
-    case 1:
-        console.log(`O valor total da conta é: R$ ${totalComDesconto.toFixed(2)} `);
+let valorDesconto = 0;
+switch (metodoPagamento){
+    case 1: 
+        valorDesconto = (totalConta*10)/100;
     break
     case 2:
-        console.log(`O valor total da conta é: R$ ${totalComDesconto.toFixed(2)} `);
+        valorDesconto = (totalConta*10)/100;
     break
     case 3:
+        valorDesconto = 0;
+    break
+    default: 
+        valorDesconto = 0;
+    return
+}
+
+
+let qtdPessoaMesa = 0;
+if (divideConta == 1){
+    qtdPessoaMesa=pessoaMesa;
+} else {
+    qtdPessoaMesa = 1;
+}
+
+
+let valorTotalConta = (totalConta+valorGorjeta-valorDesconto)/qtdPessoaMesa;
+
+
+
         console.log(`O valor total da conta é: R$ ${totalConta.toFixed(2)} `);
-    // default:
-    //     console.log("Digite um valor válido!!!")
-
-}
-
-let divideConta = parseInt(prompt("Deseja dividir o total da conta? 1 = Sim  /  2 = Não : "));
-
-if (divideConta == 1 && metodoPagamento == 1 || metodoPagamento == 2){
-    console.log(`O total para cada integrante da mesa pagar é de: " R$ ${totalComDesconto/pessoaMesa}`);
-
-}else if (divideConta == 1 && metodoPagamento == 3){
-    console.log(`O total para cada integrante da mesa pagar é de: " R$ ${totalConta/pessoaMesa}`);
-
-}else if (divideConta == 2 && metodoPagamento == 1 || metodoPagamento == 2){
-    console.log(`O total da mesa a pagar é de: " R$ ${totalComDesconto}`);
-
-}else {(divideConta == 2 && metodoPagamento == 3)
-    console.log(`O total da mesa a pagar é de: " R$ ${totalConta}`);
-}
-
-//Calcula Gorjeta
-
-let calculaGorjeta = [10,20,30,40,50];
+  
+   
+        console.log(`O valor de desconto: R$ ${valorDesconto.toFixed(2)} `);
+    
+    
+        console.log(`O valor total da gorjeta: R$ ${valorGorjeta.toFixed(2)} `);
+    
+   
+        console.log()
+    
 
 
+console.log(`O total da conta para cada pessoa:  R$ ${valorTotalConta.toFixed(2)}`);
 
-
-
-
-
-// console.log(calculaGorjeta);
